f79262177f623e20a727479306d403b930bcd2d5
iaintshine <bodziomista@gmail.com>
2017-09-12 22:05:38 +0200

    Extract constants and common methods into a module

---
+++ lib/sidekiq/tracer/commons.rb
@@ -0,0 +1,20 @@
+module Sidekiq
+  module Tracer
+    module Commons
+      def operation_name(job)
+        job['class']
+      end
+
+      def tags(job, kind)
+        {
+          'component' => 'Sidekiq',
+          'span.kind' => kind,
+          'sidekiq.queue' => job['queue'],
+          'sidekiq.jid' => job['jid'],
+          'sidekiq.retry' => job['retry'].to_s,
+          'sidekiq.args' => job['args'].join(", ")
+        }
+      end
+    end
+  end
+end
